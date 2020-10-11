bb84f9dcaaf61a193c6728ba9bd4a4d65ebdb17a
Jehru <jehru.e.harris@gmail.com>
2020-10-12 07:43:36 +1100

    General Changes
    Adding in widget so user can customise the footer text

--- wp-content/themes/wp-bootstrap-child/footer-child.php
+++ wp-content/themes/wp-bootstrap-child/footer-child.php
@@ -10,11 +10,12 @@
 			</div><!-- .row -->
 		</div><!-- .container -->
 	</div><!-- #content -->
-    <?php get_template_part( 'footer-widget' ); ?>
+	<?php //get_template_part( 'footer-widget' ); ?>
 	<footer id="colophon" class="site-footer <?php echo wp_bootstrap_starter_bg_class(); ?>" role="contentinfo">
 		<div class="container pt-3 pb-3">
             <div class="site-info">
-				<?php echo nl2br ("This website was produced by students in the Faculty of Arts & Design, University of Canberra. \n  The text and images reproduced on this site were generously provided by <a href='http://www.canberrahouse.com.au'> Canberra House </a>")  
+				<?php get_template_part( 'footer-widget' ); ?>
+				<?php //echo nl2br ("This website was produced by students in the Faculty of Arts & Design, University of Canberra. \n  The text and images reproduced on this site were generously provided by <a href='http://www.canberrahouse.com.au'> Canberra House </a>")  
 				?>  
             </div><!-- close .site-info -->
 		</div>

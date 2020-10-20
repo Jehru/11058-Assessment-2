289abf9492f92b5988232a06b31086b4aaaec6f6
Jehru <jehru.e.harris@gmail.com>
2020-10-21 09:32:49 +1100

    Changes
    - Adding in error 404 pages and category pages so that we have consistent footers in the site.
    - Adding Comments in the code
    - Styling for the home page

--- wp-content/themes/wp-bootstrap-child/functions.php
+++ wp-content/themes/wp-bootstrap-child/functions.php
@@ -1,3 +1,4 @@
 <?php 
 
+// This adds the option for an excerpt in the page document bar on the right side
 add_post_type_support( 'page', 'excerpt' );
--- wp-content/themes/wp-bootstrap-child/header-child.php
+++ wp-content/themes/wp-bootstrap-child/header-child.php
@@ -1,12 +1,9 @@
 <?php
 /**
- * The header for our theme
+ * The header is only present on the home page as other pages work fine with the parent theme
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WP_Bootstrap_Starter
  */
 
 ?><!DOCTYPE html>
